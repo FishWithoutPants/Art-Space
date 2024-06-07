d40a4e1fc15267566bda4f266e0fa3c099721980
Виктор Шуенков <shuenkov.v@office.lan>
2024-06-07 14:08:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArtSpaceTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    ArtSpaceLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceLayout() {
+    Row(
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState()),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center) {
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(50.dp)
+        ) {
+        }
+        Column(modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Surface(
+                shadowElevation = 4.dp,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.lemon_squeeze),
+                    modifier = Modifier
+                        .size(height = 400.dp, width = 250.dp)
+                        .padding(4.dp),
+                    contentScale = ContentScale.Fit,
+                    //                contentDescription = stringResource()
+                    contentDescription = "Some desc"
+                )
+            }
+            Spacer(Modifier.height(20.dp))
+            Text(text = "Header")
+            Text(text = "lower text")
+        }
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(50.dp)
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArtSpaceLayoutPreview() {
+    ArtSpaceTheme {
+        ArtSpaceLayout()
+    }
+}
